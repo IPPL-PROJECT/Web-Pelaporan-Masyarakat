651dc3025cc261044b3a05265fd29e6f91b6a0e3
aldi <aldiputrahamalsyah.com>
2025-03-17 16:21:43 +0700

    first commit

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\AuthRepository;
+use Illuminate\Support\ServiceProvider;
+use App\Repositories\ResidentRepository;
+use App\Interfaces\AuthRepositoryInterface;
+use App\Interfaces\ResidentRepositoryInterface;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app->bind(AuthRepositoryInterface::class, AuthRepository::class);
+        $this->app->bind(ResidentRepositoryInterface::class,ResidentRepository::class);
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
---
+++ app/Repositories/AuthRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Repositories;
+
+use Illuminate\Support\Facades\Auth;
+use App\Interfaces\AuthRepositoryInterface;
+
+class AuthRepository implements AuthRepositoryInterface
+{
+    public function login(array $credentials){
+      return Auth::attempt($credentials);
+    }
+
+    public function logout()
+    {
+        return Auth::logout();
+    }
+}
